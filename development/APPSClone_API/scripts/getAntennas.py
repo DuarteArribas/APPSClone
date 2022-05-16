b30c600944d8e068c0bd971f50cccda56cd98c0b
DuarteArribas <duarte.a.arribas@gmail.com>
2022-05-16 17:54:00 +0100

    added antenna getting script

---
+++ development/APPSClone_API/scripts/getAntennas.py
@@ -0,0 +1,20 @@
+#igs14_2196.atx contains all valid antennas, with a weird format.
+#antennas will contain all valid antennas in a string list format
+with open("in/antennas/igs14_2196.atx","r") as f:
+  lines             = f.readlines()
+  newLines          = []
+  antennaInNextLine = False
+  for line in lines:
+    #if the line contains an antenna, append it to the list, well formatted
+    if antennaInNextLine:
+      newLines.append(f"\"{line[0:16].strip()}\",")
+      antennaInNextLine = False
+    #if the line contains START OF ANTENNA, then the next line contains an antenna
+    if "START OF ANTENNA" in line:
+      antennaInNextLine = True
+  #the last antenna will have a comma, so we must remove it
+  lastAntenna = newLines[-1][0:len(newLines[-1])-1]
+  newLines.pop()
+  newLines.append(lastAntenna)
+  with open("out/antennas/antennas","w") as f2:
+    f2.writelines(newLines)
---
+++ development/APPSClone_API/scripts/getReceivers.py
@@ -0,0 +1,16 @@
+#GPS_Receiver_Types contains all valid receiver types, with a weird format.
+#receivers will contain all valid receiver types in a string list format.
+with open("in/receivers/GPS_Receiver_Types","r") as f:
+  lines    = f.readlines()
+  newLines = []
+  for line in lines:
+    #if the line contains a receiver and not 
+    #a comment, append it to the list, well formatted
+    if line[0] != "#":
+      newLines.append(f"\"{line[0:21].strip()}\",")
+  #the last receiver will have a comma, so we must remove it
+  lastReceiver = newLines[-1][0:len(newLines[-1])-1]
+  newLines.pop()
+  newLines.append(lastReceiver)
+  with open("out/receivers/receivers","w") as f2:
+    f2.writelines(newLines)
