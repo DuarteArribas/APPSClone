1d9bb789bf27a7ac795b20336587464aa6a146bc
DuarteArribas <duarte.a.arribas@gmail.com>
2022-05-16 17:36:41 +0100

    added receiver getting script

--- development/APPSClone_API/appsclone_api/connection.py
+++ development/APPSClone_API/appsclone_api/connection.py
@@ -3,7 +3,18 @@ from gdgps_apps.apps import APPS
 
 class Connection_APPS:
   def __init__(self,settingsFile,downloadDirectory):
-    self.apps=APPS(settings_file=settingsFile,download_directory=downloadDirectory)
+    self.apps = APPS(settings_file = settingsFile,download_directory = downloadDirectory)
 
   def testConnection(self):
     return self.apps.ping()[0]
+
+  def isRinex(self,file):
+    with open(file,"r") as f:
+      hasRinexRecLine = False
+      lines = f.readlines()
+      for line in lines:
+        if "REC # / TYPE / VERS" in line:
+          hasRinexRecLine=True
+          
+
+
