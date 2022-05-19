cc5a3087a393d3b839c595e6b768e5964a5589c7
DuarteArribas <duarte.a.arribas@gmail.com>
2022-05-19 15:38:27 +0100

    fixed formats not equal to rfcs

--- development/APPSClone_API/appsclone_api/rinexHeader.py
+++ development/APPSClone_API/appsclone_api/rinexHeader.py
@@ -15,16 +15,22 @@ class RinexHeader:
   HEADER_START              = 60
   HEADER_END                = 80
   MANDATORY_RINEX_HEADERS   = {
-    "RINEX VERSION / TYPE": "1F9.2,11X,1A1,19X,1A1,19X",
-    "PGM / RUN BY / DATE" : "3A20",
-    "MARKER NAME"         : "1A20",
-    "OBSERVER / AGENCY"   : "1A20,1A60",
+    "PGM / RUN BY / DATE" : "A20,A20,A20",
+    "MARKER NAME"         : "A60",
+    "OBSERVER / AGENCY"   : "A20,A40",
     "REC # / TYPE / VERS" : "3A20",
     "ANT # / TYPE"        : "2A20",
     "APPROX POSITION XYZ" : "3F14.4",
     "ANTENNA: DELTA H/E/N": "3F14.4",
-    "TIME OF FIRST OBS"   : "5I6,1F13.7,5X,1A3",
     "END OF HEADER"       : "60X"
+  }
+  RINEX211_VERSION_HEADER   = {
+    "RINEX VERSION / TYPE": "I6,14X,A1,19X,A1,19X",
+    "TIME OF FIRST OBS"   : "5I6,F12.6,6X,A3",
+  }
+  RINEX302_VERSION_HEADER   = {
+    "RINEX VERSION / TYPE": "F9.2,11X,A1,19X,A1,19X",
+    "TIME OF FIRST OBS"   : "5I6,F13.7,5X,A3",
   }
   # == Methods ==
   def __init__(self):
@@ -33,7 +39,7 @@ class RinexHeader:
     """
     self.header          = ""
     self.numberOfHeaders = 0
-    self.receiver        = (None,None,None)
+    self.version         = None
 
   def readMandatoryHeader(self,rinexFile):
     """Reads a rinex file's mandatory header lines and counts their number
@@ -46,6 +52,10 @@ class RinexHeader:
     with open(rinexFile,"r") as f:
       lines = f.readlines()
       for line in lines:
+        if "2.11" in line:
+          version = "2.11"
+        elif "3.02" in line:
+          version = "3.02"
         if line[RinexHeader.HEADER_START:RinexHeader.HEADER_END].strip() == "END OF HEADER":
           self.numberOfHeaders += 1
           break
@@ -53,6 +63,11 @@ class RinexHeader:
           self.numberOfHeaders += 1
           self.header          += line.strip()
           self.header          += "\n"
+        elif line[RinexHeader.HEADER_START:RinexHeader.HEADER_END].strip() == "RINEX VERSION / TYPE" or line[RinexHeader.HEADER_START:RinexHeader.HEADER_END].strip() == "TIME OF FIRST OBS":
+          self.numberOfHeaders += 1
+          self.header          += line.strip()
+          self.header          += "\n"
+        
 
   def isValidHeader(self):
     """Checks if a RINEX header is valid
@@ -78,38 +93,42 @@ class RinexHeader:
   def __isValidAntenna(self):
     pass
 
-  def parseFormat(self,line,format):
+  def __parseFormat(self,line,format):
     output          = []
+    columnsRead     = 0
     numberOfColumns = 0
     if "," in format:
       for f in format.split(","):
-        timesToLoop = __extractIntUntilString(f)
+        timesToLoop = self.__extractIntUntilString(f)
         for i in range(timesToLoop):
           if f[1] in "AIX":
-            print(f[2:])
-            numberOfColumns = int(f[2:])
+            numberOfColumns = 1 if not f[-1].isdigit() else int(f[2:])
           elif f[1] == "F":
             floatingSplit   = f[2:].split(".")
             numberOfColumns = int(floatingSplit[0])
-          output.append(line[i*numberOfColumns:i*numberOfColumns+numberOfColumns].strip())
+          output.append(line[columnsRead:columnsRead+numberOfColumns].strip())
+          columnsRead += numberOfColumns
     else:
-      timesToLoop = __extractIntUntilString(format)
+      timesToLoop = self.__extractIntUntilString(format)
       for i in range(timesToLoop):
           if format[1] in "AIX":
-            numberOfColumns = int(format[2:])
+            numberOfColumns = 1 if not format[-1].isdigit() else int(format[2:])
           elif format[1] == "F":
             floatingSplit   = format[2:].split(".")
             numberOfColumns = int(floatingSplit[0])
-          output.append(line[i*numberOfColumns:i*numberOfColumns+numberOfColumns].strip())
+          output.append(line[columnsRead:columnsRead+numberOfColumns].strip())
+          columnsRead += numberOfColumns
     return output
 
-  def extractIntUntilString(self,string):
-    if not any(char.isdigit() for char in string):
+  def __extractIntUntilString(self,string):
+    if string == "":
       return 0
+    elif not any(char.isdigit() for char in string):
+      return 1
     numberString = ""
     for i in range(len(string)):
       if not string[i].isdigit():
         break
-      else: #123
+      else:
         numberString += string[i]
-    return int(numberString)
+    return int(numberString) if numberString != "" else 1
--- development/APPSClone_API/appsclone_api/test_rinexHeader.py
+++ development/APPSClone_API/appsclone_api/test_rinexHeader.py
@@ -2,75 +2,80 @@ import unittest
 from rinexHeader import *
 
 class TestRinexheader(unittest.TestCase):
-  def test_rinex_validity1(self):
-    header = RinexHeader()
-    header.readMandatoryHeader("in/uploads/CVTY2720.21D")
-    self.assertTrue(header.isValidHeader())
+  # def test_rinex_validity1(self):
+  #   header = RinexHeader()
+  #   header.readMandatoryHeader("in/uploads/CVTY2720.21D")
+  #   self.assertTrue(header.isValidHeader())
 
-  def test_rinex_validity2(self):
-    header = RinexHeader()
-    header.readMandatoryHeader("in/uploads/fileTest")
-    self.assertFalse(header.isValidHeader())
+  # def test_rinex_validity2(self):
+  #   header = RinexHeader()
+  #   header.readMandatoryHeader("in/uploads/fileTest")
+  #   self.assertFalse(header.isValidHeader())
 
-  def test_rinex_validity3(self):
-    header = RinexHeader()
-    header.readMandatoryHeader("in/uploads/fileTest2")
-    self.assertFalse(header.isValidHeader())
+  # def test_rinex_validity3(self):
+  #   header = RinexHeader()
+  #   header.readMandatoryHeader("in/uploads/fileTest2")
+  #   self.assertFalse(header.isValidHeader())
 
   def test_string_extractor(self):
     header = RinexHeader()
-    self.assertEqual(header.extractIntUntilString("1234abc123"),1234)
+    self.assertEqual(header._RinexHeader__extractIntUntilString("1234abc321"),1234)
 
   def test_string_extractor2(self):
     header = RinexHeader()
-    self.assertEqual(header.extractIntUntilString("1234"),1234)
+    self.assertEqual(header._RinexHeader__extractIntUntilString("1234"),1234)
 
   def test_string_extractor3(self):
     header = RinexHeader()
-    self.assertEqual(header.extractIntUntilString(""),0)
+    self.assertEqual(header._RinexHeader__extractIntUntilString(""),0)
 
   def test_string_extractor4(self):
     header = RinexHeader()
-    self.assertEqual(header.extractIntUntilString("abc"),0)
+    self.assertEqual(header._RinexHeader__extractIntUntilString("abc"),1)
+
+  def test_string_extractor4(self):
+    header = RinexHeader()
+    self.assertEqual(header._RinexHeader__extractIntUntilString("abc12345"),1)
+
   # def test_rinex_format_version(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("     2.11           OBSERVATION DATA    M (MIXED)           ","1F9.2,11X,1A1,19X,1A1,19X"),["2.11",""])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("     2.11           OBSERVATION DATA    M (MIXED)           ","1F9.2,11X1,1A1,19X1,1A1,19X1"),["2.11",""])
 
   # def test_rinex_format_pgm(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("teqc  2019Feb25                         20211001 13:13:56UTC","3A20"),["teqc  2019Feb25","","20211001 13:13:56UTC"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("teqc  2019Feb25                         20211001 13:13:56UTC","3A20"),["teqc  2019Feb25","","20211001 13:13:56UTC"])
 
   # def test_rinex_format_markerName(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("CVTY                                                        ","1A20"),["CVTY"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("CVTY                                                        ","1A20"),["CVTY"])
 
   # def test_rinex_format_observerAgency(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("RFGH                SEGAL                                   ","1A20,1A60"),["RFGH","SEGAL"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("RFGH                SEGAL                                   ","1A20,1A40"),["RFGH","SEGAL"])
 
   # def test_rinex_format_receiver(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("3055025             SEPT POLARX5        5.3.2               ","3A20"),["3055025","SEPT POLARX5","5.3.2"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("3055025             SEPT POLARX5        5.3.2               ","3A20"),["3055025","SEPT POLARX5","5.3.2"])
 
   # def test_rinex_format_antenna(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("1909270030          TWIVP6050_CONE  NONE                    ","2A20"),["1909270030","TWIVP6050_CONE  NONE"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("1909270030          TWIVP6050_CONE  NONE                    ","2A20"),["1909270030","TWIVP6050_CONE  NONE"])
 
   # def test_rinex_format_approxPosXYZ(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("  6378137.0000        0.0000        0.0000                  ","3F14.4"),["6378137.0000","0.0000","0.0000"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("  6378137.0000        0.0000        0.0000                  ","3F14.4"),["6378137.0000","0.0000","0.0000"])
 
   # def test_rinex_format_antennaDeltaHEN(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("        0.9100        0.0000        0.0000                  ","3F14.4"),["0.9100","0.0000","0.0000"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("        0.9100        0.0000        0.0000                  ","3F14.4"),["0.9100","0.0000","0.0000"])
 
   # def test_rinex_format_timeOfFirstObs(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("  2021     9    29     0     0    0.0000000     GPS         ","5I6,1F13.7,5X,1A3"),["2021","0.0000","0.0000"])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("  2021     9    29     0     0    0.0000000     GPS         ","5I6,1F13.7,5X1,1A3"),["2021","0.0000","0.0000"])
 
   # def test_rinex_format_endOfHeader(self):
-  #   header = RinexHeader()
-  #   self.assertEqual(header.parseFormat("                                                            ","60X"),[""])
+  #  header = RinexHeader()
+  #  self.assertEqual(header._RinexHeader__parseFormat("                                                            ","60X1"),[""])
 
 if __name__ == '__main__':
   unittest.main()
