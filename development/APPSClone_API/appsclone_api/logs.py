058ba44053bcf41db5ee5fca642b0b0ab1093e6f
DuarteArribas <duarte.a.arribas@gmail.com>
2022-05-20 11:35:53 +0100

    documented logging class

--- development/APPSClone_API/appsclone_api/logs.py
+++ development/APPSClone_API/appsclone_api/logs.py
@@ -1,16 +1,34 @@
 import logging
 import datetime
 from enum import Enum
-
 class Logs:
+  """A logging system, that formats logs according to their severity.
+
+  Attributes
+  ----------
+  SEVERITY : enum
+    represents the logs severity in an easier to write way
+  """
+  # == Attributes ==
   SEVERITY = Enum(
     'SEVERITY', 'DEBUG INFO WARNING ERROR CRITICAL'
   )
-
+  # == Methods ==
   def __init__(self,loggingFile):
+    """Set the default configuration of the logging tool to write to a specific file with a specific format"""
     logging.basicConfig(level = logging.INFO,filename = loggingFile,format = "%(message)s")
 
   def writeLog(self,severity,message):
+    """Write a log to a file, according to its severity. Debug logs are not written, but are the default, is
+       the parameter is misspelled.
+
+    Parameters
+    ----------
+    severity : enum
+      The severity of the log
+    message  : string
+      The log message
+    """
     formattedMessage = self.__setLogMessage(severity,message)
     if severity   == Logs.SEVERITY.DEBUG:
       logging.debug(formattedMessage)
@@ -26,7 +44,14 @@ class Logs:
       logging.debug(formattedMessage)
 
   def __setLogMessage(self,severity,message):
+    """Formats the logging message, so that it stays aligned and the date, severity and message are logged.
+
+    Parameters
+    ----------
+    severity : enum
+      The severity of the log
+    message  : string
+      The log message
+    """
     severityString = str(severity).split(".")[1]
     return f"{datetime.datetime.now()} ({severityString}){' '*(8-len(severityString))} | {message}"
-
-
