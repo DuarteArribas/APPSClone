f5dffa6955df0955ee8821fb1011acb68cb5b472
YozNacks <duarte.a.arribas@gmail.com>
2022-06-14 18:06:33 +0100

    documented logs

--- development/APPSClone_API/appsclone_api/utils/logs.py
+++ development/APPSClone_API/appsclone_api/utils/logs.py
@@ -7,7 +7,7 @@ class Logs:
   Attributes
   ----------
   SEVERITY : enum
-    represents the logs severity in an easier to write way
+    Represents the logs severity in an easier to write way
   """
   # == Attributes ==
   SEVERITY = Enum(
@@ -15,18 +15,18 @@ class Logs:
   )
   # == Methods ==
   def __init__(self,loggingFile):
-    """Set the default configuration of the logging tool to write to a specific file with a specific format"""
+    """Set the default configuration of the logging tool to write to a specific file with a specific format."""
     logging.basicConfig(level = logging.INFO,filename = loggingFile,format = "%(message)s")
 
   def writeLog(self,severity,message):
-    """Write a log to a file, according to its severity. Debug logs are not written, but are the default, is
+    """Write a log to a file, according to its severity. Debug logs are not written, but are the default, if
        the parameter is misspelled.
 
     Parameters
     ----------
     severity : enum
       The severity of the log
-    message  : string
+    message  : str
       The log message
     """
     formattedMessage = self.__setLogMessage(severity,message)
@@ -50,7 +50,7 @@ class Logs:
     ----------
     severity : enum
       The severity of the log
-    message  : string
+    message  : str
       The log message
     """
     severityString = str(severity).split(".")[1]
