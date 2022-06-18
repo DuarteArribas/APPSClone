70681d062b776c94aeee2ed092de6d88c7fa96f8
YozNacks <duarte.a.arribas@gmail.com>
2022-06-18 19:13:54 +0100

    added upload file validation, by validating ipv4 and checking if the fields from the upload file are there, as well as a function to get the list of valid upload files, documented them and unit tested them

--- development/APPSClone_API/appsclone_api/fileHandler.py
+++ development/APPSClone_API/appsclone_api/fileHandler.py
@@ -1,5 +1,7 @@
 import os.path
+import re
 from os import listdir
+from vars.loggingStrings.fileHandlerLoggingStrings import *
 
 class FileHandler:
   """
@@ -7,24 +9,89 @@ class FileHandler:
   # == Attributes ==
   # == Methods ==
   @staticmethod
-  def getUploadFiles(uploadFilesDirectory):
+  def getUploadFiles(uploadFilesDirectory,logger = None):
+    """Get the list of upload files that are valid from the given directory.
+
+    Parameters
+    ----------
+    uploadFilesDirectory : str
+      The directory to read the upload files from
+    logger : Logs
+      The Logs object that will be used to log several actions
+
+    Returns
+    ----------
+    list
+      The valid upload files
+    """
     uploadFiles = os.listdir(uploadFilesDirectory)
-    uploadFiles = FileHandler.getValidatedListOfUploadFiles(uploadFiles)
-    return 
+    return [uploadFile for uploadFile in uploadFiles if FileHandler._isValidUploadFile(FileHandler._concatenateFileToPath(uploadFile,uploadFilesDirectory),logger)]
 
   @staticmethod
-  def isValidUploadFile(uploadFile):
+  def _concatenateFileToPath(file,path):
+    """Concatenate a file to a filepath, separating them by /
+
+    Parameters
+    ----------
+    file : str
+      The file to concatenate to the file path
+    path : str
+      The path to concatenate the file to
+
+    Returns
+    ----------
+    str
+      The file path, with the file included
+    """
+    return path+"/"+file
+
+  @staticmethod
+  def _isValidUploadFile(uploadFile,logger = None):
+    """Check if an upload file is valid. The first two lines must be file paths and the second line must
+    be an ipv4 address. No more lines should be in the file.
+
+    Parameters
+    ----------
+    uploadFile : str
+      The upload file to check
+
+    Returns
+    ----------
+    bool
+      True if the upload file is valid and False otherwise
+    """
     if os.path.isfile(uploadFile):
-      with open(file,"r") as f:
+      with open(uploadFile,"r") as f:
         lines = f.readlines()
-        lines = FileHandler.cleanEmptyFieldsInList(lines)
-        if lines[0].strip() != "" and lines[1].strip() != "" and lines[2].strip() != "":
-          pass
+        lines = FileHandler._cleanEmptyFieldsInList(lines)
+        if len(lines) == 3 and lines[0].strip() != "" and lines[1].strip() != "" and lines[2].strip() != "":
+          return FileHandler._isValidIpv4(lines[2])
     else:
       return False
 
   @staticmethod
-  def cleanEmptyFieldsInList(lst):
+  def _isValidIpv4(address):
+    """Check if an IPV4 address is valid.
+
+    Parameters
+    ----------
+    address : str
+      The ipv4 address to check
+
+    Returns
+    ----------
+    bool
+      True if the IPV4 address is valid and False otherwise
+    """
+    try:
+      addressBytes      = address.split('.')
+      validAddressBytes = [b for b in addressBytes if int(b) >= 0 and int(b) <= 255]
+      return len(addressBytes) == 4 and len(validAddressBytes) == 4
+    except:
+      return False
+
+  @staticmethod
+  def _cleanEmptyFieldsInList(lst):
     """Clean the empty (stripped) fields in a list.
 
     Parameters
--- development/APPSClone_API/appsclone_api/test_file_handler.py
+++ development/APPSClone_API/appsclone_api/test_file_handler.py
@@ -3,19 +3,106 @@ from fileHandler import *
 
 class TestFileHandler(unittest.TestCase):
   def test_clean_empty_strs_from_list(self):
-    self.assertEqual(FileHandler.cleanEmptyFieldsInList(["a","b","c"]),["a","b","c"])
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b","c"]),["a","b","c"])
 
   def test_clean_empty_strs_from_list2(self):
-    self.assertEqual(FileHandler.cleanEmptyFieldsInList(["a","b"," "]),["a","b"])
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b"," "]),["a","b"])
 
   def test_clean_empty_strs_from_list3(self):
-    self.assertEqual(FileHandler.cleanEmptyFieldsInList(["a","b"," ","c","   d","           "]),["a","b","c","   d"])
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b"," ","c","   d","           "]),["a","b","c","   d"])
 
   def test_clean_empty_strs_from_list4(self):
-    self.assertEqual(FileHandler.cleanEmptyFieldsInList([""]),[])
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList([""]),[])
 
   def test_clean_empty_strs_from_list5(self):
-    self.assertEqual(FileHandler.cleanEmptyFieldsInList([]),[])
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList([]),[])
+
+  def test_is_valid_ipv4(self):
+    self.assertFalse(FileHandler._isValidIpv4(""))
+
+  def test_is_valid_ipv42(self):
+    self.assertTrue(FileHandler._isValidIpv4("123.111.222.121"))
+
+  def test_is_valid_ipv43(self):
+    self.assertTrue(FileHandler._isValidIpv4("123.12.222.121"))
+
+  def test_is_valid_ipv44(self):
+    self.assertFalse(FileHandler._isValidIpv4("2.1.2.1.1"))
+
+  def test_is_valid_ipv45(self):
+    self.assertFalse(FileHandler._isValidIpv4("2.1.2.1."))
+
+  def test_is_valid_ipv46(self):
+    self.assertFalse(FileHandler._isValidIpv4("2.1.2"))
+
+  def test_is_valid_ipv47(self):
+    self.assertFalse(FileHandler._isValidIpv4("2.1.2."))
+
+  def test_is_valid_ipv48(self):
+    self.assertFalse(FileHandler._isValidIpv4("192.168.8.256"))
+
+  def test_is_valid_ipv49(self):
+    self.assertTrue(FileHandler._isValidIpv4("0.0.0.0"))
+
+  def test_is_valid_upload_file(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/arroz"))
+
+  def test_is_valid_upload_file2(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/lol"))
+
+  def test_is_valid_upload_file3(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile"))
+
+  def test_is_valid_upload_file4(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile2"))
+
+  def test_is_valid_upload_file5(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile3"))
+
+  def test_is_valid_upload_file6(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile4"))
+
+  def test_is_valid_upload_file7(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile5"))
+
+  def test_is_valid_upload_file8(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile6"))
+
+  def test_is_valid_upload_file9(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile7"))
+
+  def test_is_valid_upload_file10(self):
+    self.assertTrue(FileHandler._isValidUploadFile("in/test/uploadFile8"))
+
+  def test_is_valid_upload_file11(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile9"))
+
+  def test_is_valid_upload_file12(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile10"))
+
+  def test_is_valid_upload_file13(self):
+    self.assertTrue(FileHandler._isValidUploadFile("in/test/uploadFile11"))
+
+  def test_is_valid_upload_file14(self):
+    self.assertTrue(FileHandler._isValidUploadFile("in/test/uploadFile12"))
+
+  def test_is_valid_upload_file15(self):
+    self.assertTrue(FileHandler._isValidUploadFile("in/test/uploadFile13"))
+
+  def test_is_valid_upload_file16(self):
+    self.assertFalse(FileHandler._isValidUploadFile("in/test/uploadFile14"))
+
+  def test_is_valid_upload_file17(self):
+    self.assertTrue(FileHandler._isValidUploadFile("in/uploadFilesTest/1/uploadFile1"))
+
+  def test_concatenate_file_to_path(self):
+    self.assertEqual(FileHandler._concatenateFileToPath("arroz","aa/bb/cc"),"aa/bb/cc/arroz")
+
+  def test_get_validated_list_of_uploadFiles(self):
+    self.assertEqual(FileHandler.getUploadFiles("in/uploadFilesTest/1"),["uploadFile1","uploadFile2","uploadFile3","uploadFile4","uploadFile5"])
+
+  def test_get_validated_list_of_uploadFiles2(self):
+    self.assertEqual(FileHandler.getUploadFiles("in/uploadFilesTest/2"),["uploadFile1","uploadFile4"])
 
 if __name__ == '__main__':
   unittest.main()
