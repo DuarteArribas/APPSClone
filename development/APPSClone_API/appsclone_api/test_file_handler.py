fa5f56a54fec1127e1664d88449d1b3ec1632063
YozNacks <duarte.a.arribas@gmail.com>
2022-06-19 18:09:20 +0100

    unit tested clean empty strings from list

--- development/APPSClone_API/appsclone_api/test_file_handler.py
+++ development/APPSClone_API/appsclone_api/test_file_handler.py
@@ -5,20 +5,20 @@ class TestFileHandler(unittest.TestCase):
   def test_concatenate_file_to_path(self):
     self.assertEqual(FileHandler._concatenateFileToPath("arroz","aa/bb/cc"),"aa/bb/cc/arroz")
 
-  #def test_clean_empty_strs_from_list(self):
-  #  self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b","c"]),["a","b","c"])
+  def test_clean_empty_strs_from_list(self):
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b","c"]),["a","b","c"])
 
-  #def test_clean_empty_strs_from_list2(self):
-  #  self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b"," "]),["a","b"])
+  def test_clean_empty_strs_from_list2(self):
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b"," "]),["a","b"])
 
-  #def test_clean_empty_strs_from_list3(self):
-  #  self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b"," ","c","   d","           "]),["a","b","c","   d"])
+  def test_clean_empty_strs_from_list3(self):
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList(["a","b"," ","c","   d","           "]),["a","b","c","   d"])
 
-  #def test_clean_empty_strs_from_list4(self):
-  #  self.assertEqual(FileHandler._cleanEmptyFieldsInList([""]),[])
+  def test_clean_empty_strs_from_list4(self):
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList([""]),[])
 
-  #def test_clean_empty_strs_from_list5(self):
-  #  self.assertEqual(FileHandler._cleanEmptyFieldsInList([]),[])
+  def test_clean_empty_strs_from_list5(self):
+    self.assertEqual(FileHandler._cleanEmptyFieldsInList([]),[])
 
   #def test_is_valid_ipv4(self):
   #  self.assertFalse(FileHandler._isValidIpv4(""))
