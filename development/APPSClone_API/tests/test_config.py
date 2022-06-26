eeab926c735d7a16988030431331313ef5fce98e
YozNacks <duarte.a.arribas@gmail.com>
2022-06-26 18:42:06 +0100

    unit tested config settings and change configuration

--- development/APPSClone_API/tests/test_config.py
+++ development/APPSClone_API/tests/test_config.py
@@ -3,9 +3,13 @@ import unittest
 from appsclone_api.utils.config import *
 
 class TestConfig(unittest.TestCase):
-  def test_get_filePath(self):
+  def test_get_logs(self):
     c = Config("config/appsclone.cfg")
-    print(c.getLogConfig("LOGS_FILE_PATH"))
+    self.assertEqual(c.getLogConfig("LOGS_FILE"),"logs/logs.log")
+
+  def test_get_settings(self):
+    c = Config("config/appsclone.cfg")
+    self.assertEqual(c.getSettingsConfig("APPS_SETTINGS_FILE"),"config/apps_settings")
 
 if __name__ == '__main__':
   unittest.main()
