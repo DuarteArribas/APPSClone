b796630f7f5c98a10d2395c4db667c237eeec1b6
YozNacks <duarte.a.arribas@gmail.com>
2022-06-28 10:35:32 +0100

    added input function, divided into print title, print ctrl g and get input box input functions. Also added print input error function. Documented all functions and class and unit tested all functions

--- development/APPSClone_Client/appsclone_client/gui.py
+++ development/APPSClone_Client/appsclone_client/gui.py
@@ -2,42 +2,86 @@ import curses
 from curses.textpad import Textbox,rectangle
 
 class GUI:
+  """GUI interface using curses."""
   def __init__(self):
+    """Initialize curses."""
     self.stdscr = curses.initscr()
     curses.start_color()
     curses.cbreak()
     curses.echo(False)
-    # colors
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_YELLOW)
-    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_GREEN)
-    # some color pairs
-    self.white_red = curses.color_pair(1)
-    self.red_black = curses.color_pair(2)
-    self.black_yellow = curses.color_pair(3)
-    self.white_green = curses.color_pair(4)
-
-  def printTitle(self,title):
-    self.stdscr.erase()
-    self.stdscr.addstr(2,2,title,curses.A_BOLD | self.red_black)
-    self.stdscr.refresh()
-
-  def getInput(self,title):
-    self.printTitle(title)
-    newWindow = curses.newwin(10,40,4,3)
-    self.stdscr.addstr(16,3,"CTRL+G to submit",curses.A_BOLD)
-    box = Textbox(newWindow)
-    rectangle(self.stdscr,3,2,14,43)
-    rectangle(self.stdscr,15,2,17,19)
-    self.stdscr.refresh()
-    box.edit()
-    return box.gather()
-
-  def wait(self):
-    self.stdscr.getch()
+    curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_WHITE,curses.COLOR_RED)
+    self.greenBlack = curses.color_pair(1)   
+    self.whiteRed = curses.color_pair(2)
 
   def __del__(self):
+    """Return the terminal to a santizied state."""
     curses.nocbreak()
     curses.echo()
-    curses.endwin()
+    curses.endwin() 
+
+  def getInput(self,title):
+    """Get an input from the stdin.
+
+    Parameters
+    ----------
+    title : str
+      The title to show at the top of the input box
+
+    Returns
+    ----------
+    str
+      The input stripped and without any new line characters
+    """
+    self.stdscr.erase()
+    self.__printTitle(title)
+    self.__printCtrlG()
+    return self.__getInputBoxInput()
+
+  def __printTitle(self,title):
+    """Print the title to the specific coordinates to the stdout.
+
+    Parameters
+    ----------
+    title : str
+      The title to print
+    """
+    self.stdscr.addstr(2,3,title,curses.A_BOLD | self.greenBlack)
+    self.stdscr.refresh()
+
+  def __printCtrlG(self):
+    """Print the CTRL + G confirmation at the specific coordinates, alongside its rectangle to the stdout."""
+    self.stdscr.addstr(16,3,"CTRL+G to submit",curses.A_BOLD)
+    rectangle(self.stdscr,15,2,17,19)
+    self.stdscr.refresh()
+
+  def __getInputBoxInput(self):
+    """Get an input from the input box from the stdin.
+    
+    Returns
+    ----------
+    str
+      The input stripped and without any new line characters
+    """
+    inputWindow = curses.newwin(10,40,4,3)
+    box         = Textbox(inputWindow)
+    rectangle(self.stdscr,3,2,14,43)
+    self.stdscr.refresh()
+    box.edit()
+    return box.gather().strip().replace("\n","")
+
+  def printInputError(self,errorMsg):
+    """Print an error message for the input.
+
+    Parameters
+    ----------
+    errorMsg : str
+      The error message to print
+    """
+    errorWindow = curses.newwin(3,22,15,21)
+    errorWindow.addstr(errorMsg,curses.A_BOLD | self.whiteRed)
+    errorWindow.refresh()
+
+  def waitTest(self):
+    """Wait for input. Only used for testing purposes."""
+    self.stdscr.getch()
---
+++ development/APPSClone_Client/tests/test_gui.py
@@ -0,0 +1,30 @@
+import appsclone_client
+import unittest
+from appsclone_client.gui import *
+
+class TestGUI(unittest.TestCase):
+  # def test_print_title(self):
+  #   g = GUI()
+  #   g._GUI__printTitle("arroz") 
+  #   g.waitTest()
+
+  # def test_print_ctrl_g(self):
+  #   g = GUI()
+  #   g._GUI__printCtrlG() 
+  #   g.waitTest()
+
+  # def test_print_input_box_rectangle(self):
+  #   g = GUI()
+  #   g._GUI__getInputBoxInput() 
+  #   g.waitTest()
+
+  # def test_get_input(self):
+  #   g = GUI()
+  #   self.assertEqual(g.getInput("What's your favorite color?"),"green")
+
+  # def test_showInputError(self):
+  #   g = GUI()
+  #   g.printInputError("arroz lol")
+
+if __name__ == '__main__':
+  unittest.main()
